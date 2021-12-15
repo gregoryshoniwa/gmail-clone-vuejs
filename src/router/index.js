0d0e375d8216b62b9b2076ca463de4e11fb87b7f
gregoryshoniwa <gregoryshoniwa@gmail.com>
2021-12-15 12:45:12 +0200

    login page done

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,27 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/signup',
+    name: 'Signup',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+  },
+  
+  {
+    path: '/forgot',
+    name: 'Forgot',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Forgot.vue')
   }
 ]
 
---
+++ src/views/Login.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="home">
+      <div class="container">
+          <div class="inner-container">
+                <img alt="Google logo" src="@/assets/logo.png">
+                <div class="signin-text">Sign In</div>
+                <div class="signin-text-continue">to continue to gmail</div>
+                <el-input class="email" v-model="email" placeholder="Email or Phone Number" clearable />
+                <el-input class="password" v-model="password" placeholder="Password" show-password />
+                <div class="forgot-email">Forgot email?</div>
+                <div class="not-your-computer">Not your computer? Use guest mode to signin privately.</div>
+                <div class="learn-more">Learn more</div>
+                <div class="button-container">
+                    <el-button class="create-account-button" type="text">Create Account</el-button>
+                    <el-button class="login-button" type="primary">Login</el-button>
+                </div>
+                
+            </div>
+       </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  data(){
+      return{
+          email: '',
+          password: '',
+      }
+  }
+}
+</script>
+<style scoped>
+    img{
+        width:120px;
+        height:auto
+    }
+    .home {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 100px;
+        display:flex;
+        justify-content: center;
+        
+    }
+    .container{
+        border: 1px solid rgb(207, 207, 207);
+        border-radius: 5px;
+        width: 30%;
+        margin: 10px;
+    }
+    .inner-container{
+        margin: 30px;
+    }
+
+    .signin-text{
+         
+         font-size: 25px;
+    }
+    .signin-text-continue{
+        margin-top: 10px;
+    }
+    .email{
+         margin-top: 30px;
+    }
+    .password{
+         margin-top: 30px;
+    }
+    .forgot-email{
+       color: rgb(105, 108, 255);
+       display: flex;
+       margin-top: 10px;
+       cursor: pointer;
+    }
+    .not-your-computer{
+         margin-top: 30px;
+         display: flex;
+         
+    }
+    .learn-more{
+        color: rgb(105, 108, 255);
+        margin-bottom: 30px;
+        display: flex;
+        cursor: pointer;
+    }
+    .create-account-button{
+        margin-top: 20px;
+    }
+    .login-button{
+       margin-top: 20px;
+    }
+    .button-container{
+        display: flex;
+        justify-content: space-between;
+    }
+
+    
+</style>
